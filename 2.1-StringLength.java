5ead8970237688743f371e21677865a1d1e32f15
BURAK KAAN ÖZDEMİR <ozdemirburakkaan@gmail.com>
2026-02-13 16:00:08 +0300

    Delete 2.1-StringLength.java

--- 2.1-StringLength.java
+++
@@ -1,9 +0,0 @@
-public class StringLength {
-    
-  public static void main(String[] args) {
-        String text = "Java Learning";
-        
-        // length() → counts total characters in the string
-        System.out.println("Length: " + text.length());
-    }
-}
